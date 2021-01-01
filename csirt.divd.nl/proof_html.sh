37f2091bb8ca3710023365b97a3e7774154e7d3a
Frank Breedijk <f.breedijk@divd.nl>
2021-01-01 10:54:42 +0100

    Adding case 2020-00013 (#6)
    * Adding cases 2020-00013

--- csirt.divd.nl/proof_html.sh
+++ csirt.divd.nl/proof_html.sh
@@ -2,4 +2,4 @@
 gem install html-proofer
 htmlproofer \
 	--allow-hash-href  \
-	--url-ignore="/www.linkedin.com/","/twitter.com/","/#english/","/x1sec.com/" _site
+	--url-ignore="/www.linkedin.com/","/twitter.com/","/www.infoo.nl/","/#english/","/x1sec.com/" _site
