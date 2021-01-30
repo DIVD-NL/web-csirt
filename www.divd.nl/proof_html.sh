1f8c94f3bcd268c831418c06138dc055837d7091
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-01-30 17:12:21 +0100

    Fixing link checker so changes get pushed again

--- www.divd.nl/proof_html.sh
+++ www.divd.nl/proof_html.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 gem install html-proofer
+echo "*** Internal link check ***"
+htmlproofer \
+	--disable_external \
+	--allow-hash-href  \
+	--url-ignore="/#menu/" _site
+echo "*** External link check ***"
 htmlproofer \
 	--allow-hash-href \
-	--url-ignore="/www.linkedin.com/","/twitter.com/","/#menu/" _site
+	--url-ignore="/www.linkedin.com/","/twitter.com/","/#menu/" _site || exit 0
--- www.divd.nl/test_it.sh
+++ www.divd.nl/test_it.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 docker pull mrseccubus/github-pages:latest
-while [ 1 ] ; do
-	docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
-	-c "
-		cd /root/project;
-		rm -rf _site/*;
-		jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning';
-		./proof_html.sh
-	"
-	set -e
-	echo 'Press CTRL+C now to quit'
-	sleep 1
-	set +e
-	echo 'Restarting...'
-done
+docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
+-c "
+	cd /root/project;
+	rm -rf _site/*;
+	jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning';
+	./proof_html.sh
+"
 
