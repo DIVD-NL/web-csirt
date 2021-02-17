dc962c8bf72c0369ed70debf895a61593e889f99
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-02-17 13:46:04 +0100

    Fixed syntax error

--- csirt.divd.nl/proof_html.sh
+++ csirt.divd.nl/proof_html.sh
@@ -3,7 +3,8 @@ TEAMCOUNT_HERE=$( ls _team|wc -l )
 TEAMCOUNT_THERE=$( ls ../www.divd.nl/_team|wc -l )
 if [[ TEAMCOUNT_HERE -le 0 || TEAMCOUNT_HERE -ne TEAMCOUNT_THERE ]]; then
 	echo "_team directory is not updated, run ./update.sh"
-figem install html-proofer
+fi
+gem install html-proofer
 echo "*** Internal link check ***"
 htmlproofer \
 	--disable_external \
