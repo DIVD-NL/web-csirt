da2b94ca7a125ad661b709a377d9f88927d69a43
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-05-10 23:27:41 +0200

    Fixed eror with link checking

--- csirt.divd.nl/proof_html.sh
+++ csirt.divd.nl/proof_html.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e # Need to fail on error
-TEAMCOUNT_HERE=$( ls _team|wc -l )
-TEAMCOUNT_THERE=$( ls ../www.divd.nl/_team|wc -l )
-if [[ TEAMCOUNT_HERE -le 0 || TEAMCOUNT_HERE -ne TEAMCOUNT_THERE ]]; then
-	echo "_team directory is not updated, run ./update.sh"
-fi
+#TEAMCOUNT_HERE=$( ls _team|wc -l )
+#TEAMCOUNT_THERE=$( ls ../www.divd.nl/_team|wc -l )
+#if [[ TEAMCOUNT_HERE -le 0 || TEAMCOUNT_HERE -ne TEAMCOUNT_THERE ]]; then
+#	echo "_team directory is not updated, run ./update.sh"
+#fi
 gem install html-proofer
 echo "*** Internal link check ***"
 htmlproofer \
@@ -12,6 +12,6 @@ htmlproofer \
 	--allow-hash-href  \
 	--url-ignore="/#english/" _site
 echo "*** External link check ***"
-htmlproofer \
+(set +e ; htmlproofer \
 	--allow-hash-href \
-	--url-ignore="/www.linkedin.com/","/twitter.com/","/www.infoo.nl/","/#english/","/x1sec.com/" _site || exit 0
+	--url-ignore="/www.linkedin.com/","/twitter.com/","/www.infoo.nl/","/#english/","/x1sec.com/" _site || exit 0)
