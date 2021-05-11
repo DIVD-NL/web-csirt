2ee775d98cbf0c22591e2d18fb050f3c8f2541e6
Frank Breedijk <f.breedijk@divd.nl>
2021-05-11 11:53:25 +0200

    Vembu limited disclosure (#39)
    * nieuwe post Vembu
    
    * .md toegevoegd
    
    * Limited disclosure Vembu
    
    * nieuwe tekst
    
    * opmerkingen Frank verwerkt, nieuwe tekst
    
    * nog een paar aanpassingen
    
    * There's more?
    
    * Better proofing
    
    * Make sure we have a _team directory
    
    * Better escaping (Regression)
    
    * apt not apt-get
    
    * Updated blogpost
    
    Co-authored-by: gerardaloha <g.janssen@divd.nl>

--- csirt.divd.nl/update.sh
+++ csirt.divd.nl/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#echo "Updating team"
-#(
-#	cd _team
-#	rm -f *.md
-#	cp ../../www.divd.nl/_team/*.md .
-#)
+echo "Updating team"
+(
+	cd _team
+	rm -f *.md
+	cp ../../www.divd.nl/_team/*.md .
+)
