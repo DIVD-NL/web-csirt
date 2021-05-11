1e23fd2ae5c3eb2f332804985a5309f9b5e738ad
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-05-11 11:09:38 +0200

    Better proofing

--- csirt.divd.nl/proof_html.sh
+++ csirt.divd.nl/proof_html.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e # Need to fail on error
-#TEAMCOUNT_HERE=$( ls _team|wc -l )
-#TEAMCOUNT_THERE=$( ls ../www.divd.nl/_team|wc -l )
-#if [[ TEAMCOUNT_HERE -le 0 || TEAMCOUNT_HERE -ne TEAMCOUNT_THERE ]]; then
-#	echo "_team directory is not updated, run ./update.sh"
-#fi
+TEAMCOUNT_HERE=$( ls _team|wc -l )
+TEAMCOUNT_THERE=$( ls ../www.divd.nl/_team|wc -l )
+if [[ TEAMCOUNT_HERE -le 0 || TEAMCOUNT_HERE -ne TEAMCOUNT_THERE ]]; then
+	echo "_team directory is not updated, run ./update.sh"
+	exit 1
+fi
 gem install html-proofer
 echo "*** Internal link check ***"
 htmlproofer \
