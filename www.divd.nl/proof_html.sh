66853f16326f13bb83a48932ef146497009d1c89
Frank Breedijk <fbreedijk@schubergphilis.com>
2021-11-11 16:18:23 +0100

    This should fix PR #155 and make it run again

--- www.divd.nl/proof_html.sh
+++ www.divd.nl/proof_html.sh
@@ -2,19 +2,19 @@
 set -e
 CASECOUNT_HERE=$( ls _cases|wc -l )
 CASECOUNT_THERE=$( ls ../csirt.divd.nl/cases|wc -l )
-if [[ CASECOUNT_HERE -le 0 || CASECOUNT_HERE -ne CASECOUNT_THERE ]]; then
+if [[ $CASECOUNT_HERE -le 0 || $CASECOUNT_HERE -ne $CASECOUNT_THERE ]]; then
 	echo "_cases directory is not updated, run ./update.sh"
 	exit 1
 fi
 POSTCOUNT_HERE=$( ls _csirt_posts|wc -l )
 POSTCOUNT_THERE=$( ls ../csirt.divd.nl/_posts|wc -l )
-if [[ POSTCOUNT_HERE -le 0 || POSTCOUNT_HERE -ne POSTCOUNT_THERE ]]; then
+if [[ $POSTCOUNT_HERE -le 0 || $POSTCOUNT_HERE -ne $POSTCOUNT_THERE ]]; then
 	echo "_csirt_posts directory is not updated, run ./update.sh"
 	exit 1
 fi
 CVECOUNT_HERE=$( ls _cves|wc -l )
 CVECOUNT_THERE=$( ls ../csirt.divd.nl/_cves|wc -l )
-if [[ CVECOUNT_HERE -le 0 || CVECOUNT_HERE -ne CVECOUNT_THERE ]]; then
+if [[ $CVECOUNT_HERE -le 0 || $CVECOUNT_HERE -ne $CVECOUNT_THERE ]]; then
 	echo "_cves directory is not updated, run ./update.sh"
 	exit 1
 fi
