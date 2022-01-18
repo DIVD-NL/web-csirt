7e13afad24b816569842b5cf408819b6c4c1a3db
Frank Breedijk <fbreedijk@schubergphilis.com>
2022-01-18 16:53:19 +0100

    Make sure missing exceprts on cases don't break HTML

--- csirt.divd.nl/test_it.sh
+++ csirt.divd.nl/test_it.sh
@@ -4,8 +4,8 @@ docker pull mrseccubus/github-pages:latest
 docker run --volume="$PWD/..:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
 -c "
 	cd /root/project/csirt.divd.nl;
+	./update.sh;
 	rm -rf _site/*;
 	jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning';
-	./update.sh
 	./proof_html.sh
 "
