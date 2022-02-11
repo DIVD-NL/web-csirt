049136a289075f36ea95f06b659faf29a71e921d
SchizoDuckie <SchizoDuckie@users.noreply.github.com>
2022-02-11 22:02:46 +0100

    applied some 2022 spit-shine to the javascript

--- www.divd.nl/assets/js/supp.js
+++ www.divd.nl/assets/js/supp.js
@@ -1,71 +1,45 @@
 /*
- * Functions for the donate page
+ * Functions for the donate page: 
+ * - Fetch the data
+ * - Render the html.
  */
 
-function update_display(data) {
-	$('#name').text(data.name);
-	$('#description').text(data.description);
-	url = data.absoluteUrl;
-	$(".donate_btn").attr("url",url);
-	$('#amount').text(data.raised);
-	if (data.goal > 0) {
-		$('#goal').text("of €" + data.goal);
-	} else { 
-		$('#goal').text("");
-	}
-	data.donations.forEach(function(d) {
-		txt = "<tr>"
-		txt += "<td>"+$("<div>").text(d.created_at.split(" ")[0].split("-").reverse().join("-")).html()+"</td>"
-		txt += "<td class='right-align'>&euro; "+$("<div>").text(d.amount).html()+"</td>"
-		if( d.anonymous ) {
-			txt += "<td>Anonymous</td>"
-		} else { 
-			var name = ""
-			if(d.first_name != null) {
-				name = d.first_name
-			}
-			if(d.last_name_prefix != null) {
-				name = name + " " + d.last_name_prefix
-			}
-			if(d.last_name != null) {
-				name = name + " " + d.last_name
-			}
-			txt += "<td>"+$("<div>").text(name).html()+"</td>"
-		}
-		txt += "<td>"+$("<div>").text(d.message).html()+"</td>"
-		txt += "</tr>"
-		$('#donations').prepend(txt)
-
-	})
-	$('.donate_btn').on('click', function(evt) {
-		v=evt.target.value;
-		if(v) {
-			window.location.href=data.absoluteUrl+"/donate/"+v
-		} else {
-			window.location.href=data.absoluteUrl+"/donate"
-		}
-	});
+/**
+ * Anti-XSS condom
+ */
+function condom(unsafe) { 
+	return $("<div>").text(unsafe).html(); 
 }
 
-function load_donations() {
-	$.ajax({
+/**
+ * Iterate donations in reverse (to render the latest ones first and then:
+ * - fix the date format and breaking on '-' in the table by replacing it with &#8209;
+ * - Parse the name, or flag as anonymous
+ * - html-safe render the name and donation comment.
+ * - add the referal data to the donate link
+ */
+function renderDonations(data) {  
+    
+    let rows = data.donations.reverse().map(function(donation) {
+        let created = donation.created_at.split(" ")[0].split("-").reverse().join("&#8209;");
+        let name = donation.anonymous ? "Anonymous" :  `${donation.first_name ?? ''} ${donation.last_name_prefix ?? ''} ${donation.last_name ?? ''}`;
+        
+        return `<tr><td>${created}</td>
+                    <td class='right-align'>&euro; ${donation.amount}</td>
+                    <td>${condom(name)}</td>
+                    <td>${condom(donation.message)}</td>
+        </tr>`;
+    });
+    
+    $("#donations").html(rows.join(''));
 
-	    url : '/data/supp.json',
-	    type : 'GET',
-	    data : {},
-	    dataType:'json',
-	    success : function(data) {
-	    	update_display(data.data)   
-	    },
-	    error : function(request,error)
-	    {
-	    	console.error("Error: "+ error)
-	        console.log("Request: "+JSON.stringify(request));
-	    }
-	});
+    $('.donate_btn').on('click', function(evt) {
+	let addition = evt.target.value ? "/v=${evt.target.value}" : '';
+	window.location.href = data.absoluteUrl+"/donate${addition}";
+    });
+
 };
 
-
-$( document ).ready(function() {
-	load_donations()
-})
+$(document).ready(function() {
+	$.getJSON('/data/supp.json').then((result)=>renderDonations(result.data));
+});
