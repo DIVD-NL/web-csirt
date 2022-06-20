2c933e85120b8342a68c35687cbfc47625c72bdd
Frank Breedijk <fbreedijk@schubergphilis.com>
2022-06-20 08:17:49 +0200

    Fixing proofer

--- proof_html.sh
+++ proof_html.sh
@@ -7,19 +7,19 @@ apt install python3-pip libcurl4 -y
 pip3 install html5validator 
 
 CASECOUNT_HERE=$( ls _cases|wc -l )
-CASECOUNT_THERE=$( ls ../csirt.divd.nl/_cases|wc -l )
+CASECOUNT_THERE=$( ls csirt.divd.nl/_cases|wc -l )
 if [[ $CASECOUNT_HERE -le 0 || $CASECOUNT_HERE -ne $CASECOUNT_THERE ]]; then
 	echo "_cases directory is not updated, run ./update.sh"
 	exit 1
 fi
 POSTCOUNT_HERE=$( ls _csirt_posts|wc -l )
-POSTCOUNT_THERE=$( ls ../csirt.divd.nl/_posts|wc -l )
+POSTCOUNT_THERE=$( ls csirt.divd.nl/_posts|wc -l )
 if [[ $POSTCOUNT_HERE -le 0 || $POSTCOUNT_HERE -ne $POSTCOUNT_THERE ]]; then
 	echo "_csirt_posts directory is not updated, run ./update.sh"
 	exit 1
 fi
 CVECOUNT_HERE=$( ls _cves|wc -l )
-CVECOUNT_THERE=$( ls ../csirt.divd.nl/_cves|wc -l )
+CVECOUNT_THERE=$( ls csirt.divd.nl/_cves|wc -l )
 if [[ $CVECOUNT_HERE -le 0 || $CVECOUNT_HERE -ne $CVECOUNT_THERE ]]; then
 	echo "_cves directory is not updated, run ./update.sh"
 	exit 1
--- test_it.sh
+++ test_it.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker pull mrseccubus/github-pages:latest
-docker run --volume="$PWD/..:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
+docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
 -c "
-	cd /root/project/www.divd.nl;
+	cd /root/project/;
 	./update.sh;
 	rm -rf _site/*;
 	jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning';
