44603766ca3e54208a136391b97fbdaa8215700a
Frank Breedijk <f.breedijk@divd.nl>
2022-10-05 19:41:59 +0200

    Moving some stuff around

--- test_it.sh
+++ test_it.sh
@@ -4,9 +4,17 @@ docker pull mrseccubus/github-pages:latest
 docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
 -c "
 	cd /root/project/;
+	if [[ -e Gemfile ]]; then
+		echo \"*** Installing Gems in Gemfile ***\"
+		bundle install 
+		EXEC=\"bundle exec\"
+	fi
 	git config --global --add safe.directory /root/project/www.divd.nl
 	./update.sh;
 	rm -rf _site/* jekyll-build.log;
-	jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning' 2>&1 | tee jekyll-build.log
+	\$EXEC jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning' 2>&1 | tee jekyll-build.log
+	apt-get update -y
+	apt-get install python3-pip libcurl4 -y
+	pip3 install html5validator 
 	./proof_html.sh
 "
